fed16b12dc0d5667e8317beb6da24acab9bc6291
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-18 20:48:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
   def self.new_by_filename(filename)
     file = filename.split(" - ")
     @song = self.new(file[1])
-    @artist = self.new(file[0])
-    @song.artist = @artist
+    @Artist = self.new(file[0])
+    @song.artist = @Artist
     @@all << @song
     @song
   end
