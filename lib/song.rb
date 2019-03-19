9009f7bfb8c1977106f7c5f11d30a34ee3bc57ee
Henry Sztanski <henry@sztanski.com>
2019-03-19 14:07:18 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,9 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    #Artist.new('Michael Jackson')
+    song.artist = Artist.new(filename.split(" - ")[0])
+    song.artist.add_song(song)
+    song
   end
 
   def artist_name=(name)
